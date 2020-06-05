9efcabab102e15d4423b40d6746ed8f18cbf4702
Samuel RS <34385530+SamuelSuriel@users.noreply.github.com>
2020-06-05 14:07:53 -0400

    Add shared/common files

---
+++ LightControlPro/LoginPage.xaml.cs
@@ -0,0 +1,78 @@
+﻿using LightControlPro.Models;
+using LightControlPro.Views;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LightControlPro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            Init();
+        }
+        void Init()
+        {
+            //BackgroundImage = "icon.png";
+            //BackgroundColor = Constants.BackgroundColor;
+            //Lbl_Username.TextColor = Constants.MainTextColor;
+            //Lbl_Password.TextColor = Constants.MainTextColor;
+            //icon.HeightRequest = Constants.iconHeight;
+            Entry_Username.Completed += (s, e) => Entry_Password.Focus();
+            Entry_Password.Completed += (s, e) => SignInProcedureAsync(s, e);
+            Btn_Signin.IsEnabled = true;
+        }
+        async void SignInProcedureAsync(object sender, EventArgs e)
+        {
+            //if (Entry_Username.Text == string.Empty & Entry_Password.Text == string.Empty)
+            if (Entry_Username.Text == null && Entry_Password.Text == null)
+            {
+                await DisplayAlert("Message", "Please fill the user and password", "OK");
+            }
+            else
+            {
+                User user = new User(Entry_Username.Text, Entry_Password.Text);
+                if (user.CheckInformation())
+                {
+                    //ActivitySpinner.IsVisible = false;
+                    //var result = await App.RestService.Login(user);
+                    //result = new Token();
+
+                    await DisplayAlert("Login", "Login Success!", "OK");
+                    //if (result != null)
+                    //{
+                    //    App.UserDatabase.SaveUser(user);
+                    //    App.TokenDatabase.SaveToken(result);
+                    //}
+                    if (Device.OS == TargetPlatform.Android)
+                    {
+                        //Application.Current.MainPage = (new NavigationPage(new Account()));
+                    }
+                    else if (Device.OS == TargetPlatform.iOS)
+                    {
+                        //await Navigation.PushAsync(new NavigationPage (new Account()));
+                    }
+                    else
+                    {
+                        await DisplayAlert("Login", "Login Not Correct, empty username or password.", "OK");
+                        //ActivitySpinner.IsVisible = false;
+                    }
+
+                }
+            }
+        }
+
+        //private void ButtonSkip_Clicked(object sender, EventArgs e)
+        //{
+        //    Navigation.PushModalAsync(new MainPage());
+        //}
+    }
+}
---
+++ LightControlPro/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using LightControlPro.MenuItems;
+using System;
+using LightControlPro.Views;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LightControlPro
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : MasterDetailPage
+    {
+        public List<MasterPageItem> menuList { get; set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            menuList = new List<MasterPageItem>();
+            // Creating our pages for menu navigation
+            // Here you can define title for item, 
+            // icon on the left side, and page that you want to open after selection
+            var page1 = new MasterPageItem() { Title = "Home", Icon = "ic_lcp3.png", TargetType = typeof(ViewMain) };
+            var page2 = new MasterPageItem() { Title = "SavingTips", Icon = "ic_lcp6.png", TargetType = typeof(SavingTips) };
+            var page3 = new MasterPageItem() { Title = "About", Icon = "ic_lcp7.png", TargetType = typeof(About) };
+            //var page4 = new MasterPageItem() { Title = "Support", Icon = "ic_lcp9.png", TargetType = typeof(Chat) };
+            //var page5 = new MasterPageItem() { Title = "Settings", Icon = "ic_lcp5.png", TargetType = typeof(Settings) };
+            var page6 = new MasterPageItem() { Title = "Account", Icon = "ic_lcp8.png", TargetType = typeof(Account) };
+
+
+            // Adding menu items to menuList
+            menuList.Add(page1);
+            menuList.Add(page2);
+            menuList.Add(page3);
+            //menuList.Add(page4);
+            //menuList.Add(page5);
+            menuList.Add(page6);
+
+
+            // Setting our list to be ItemSource for ListView in MainPage.xaml
+            navigationDrawerList.ItemsSource = menuList;
+            // Initial navigation, this can be used for our home page
+            Detail = new NavigationPage((Page)Activator.CreateInstance(typeof(ViewMain)));
+            this.BindingContext = new
+            {
+                Header = "",
+                Image = "ic_launcher.png",
+                //Footer = "         -------- User --------           "
+                Footer = "User"
+            };
+        }
+
+
+
+        private void OnMenuItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var item = (MasterPageItem)e.SelectedItem;
+            Type page = item.TargetType;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(page));
+            //Detail.Navigation.PushAsync((Page)Activator.CreateInstance(page));
+            IsPresented = false;
+        }
+    }
+}
